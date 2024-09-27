8381581510907a38801ad875c64db5b31574a8be
Angel Santamaria Navarro <somriu@gmail.com>
2024-09-27 14:21:45 +0200

    fix parameters and launch file. working U22.04 and ROS Humble

--- launch/run.launch.py
+++ launch/run.launch.py
@@ -9,69 +9,57 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
     share_dir = get_package_share_directory('lio_sam')
-    parameter_file = LaunchConfiguration('params_file')
-    xacro_path = os.path.join(share_dir, 'config', 'robot.urdf.xacro')
+    parameter_file = os.path.join(
+      get_package_share_directory('lio_sam'),
+      'config',
+      'params.yaml'
+      )
     rviz_config_file = os.path.join(share_dir, 'config', 'rviz2.rviz')
 
-    params_declare = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(
-            share_dir, 'config', 'params.yaml'),
-        description='FPath to the ROS2 parameters file to use.')
-
-    print("urdf_file_name : {}".format(xacro_path))
-
-    return LaunchDescription([
-        params_declare,
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            arguments='0.0 0.0 0.0 0.0 0.0 0.0 map odom'.split(' '),
-            parameters=[parameter_file],
-            output='screen'
-            ),
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{
-                'robot_description': Command(['xacro', ' ', xacro_path])
-            }]
-        ),
-        Node(
+    lio_sam_imuPreintegration = Node(
             package='lio_sam',
             executable='lio_sam_imuPreintegration',
             name='lio_sam_imuPreintegration',
             parameters=[parameter_file],
             output='screen'
-        ),
-        Node(
+        )
+    
+    lio_sam_imageProjection = Node(
             package='lio_sam',
             executable='lio_sam_imageProjection',
             name='lio_sam_imageProjection',
             parameters=[parameter_file],
             output='screen'
-        ),
-        Node(
+        )
+    
+    lio_sam_featureExtraction = Node(
             package='lio_sam',
             executable='lio_sam_featureExtraction',
             name='lio_sam_featureExtraction',
             parameters=[parameter_file],
             output='screen'
-        ),
-        Node(
+        )
+    
+    lio_sam_mapOptimization = Node(
             package='lio_sam',
             executable='lio_sam_mapOptimization',
             name='lio_sam_mapOptimization',
             parameters=[parameter_file],
             output='screen'
-        ),
-        Node(
+        )
+    
+    rviz2 = Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
             arguments=['-d', rviz_config_file],
             output='screen'
         )
-    ])
+
+    ld = LaunchDescription()
+    ld.add_action(lio_sam_imuPreintegration)
+    ld.add_action(lio_sam_imageProjection)
+    ld.add_action(lio_sam_featureExtraction)
+    ld.add_action(lio_sam_mapOptimization)
+    ld.add_action(rviz2)
+    return ld
